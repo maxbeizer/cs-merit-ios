b2ae4853a1f9aaa33c809524271a4c4df4f84e91
Max Beizer <max.beizer@gmail.com>
2015-03-10 20:51:44 -0500

    Initial commit

---
+++ cs-merit-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  cs-merit-ios
+//
+//  Created by Max Beizer on 3/10/15.
+//  Copyright (c) 2015 Max Beizer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
