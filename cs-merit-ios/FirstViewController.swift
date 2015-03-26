26de6c8792fda9376d441e5298b4f4b74f30b33f
Max Beizer <max.beizer@gmail.com>
2015-03-25 20:41:00 -0500

    Add hardcoded call to api with AF and SwiftyJSON

--- cs-merit-ios/FirstViewController.swift
+++ cs-merit-ios/FirstViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+      Alamofire.request(.GET, "http://merit.centresource.com/api/v1/badge-types", parameters: ["user_email": "mbeizer@centresource.com", "user_token": "SwDXw3x_gQrjyk_55sXK"])
+        .responseJSON() {
+          (_, _, data, _) in
+          let foo = JSON(data!)
+          println(data)
+        }
     }
 
     override func didReceiveMemoryWarning() {
