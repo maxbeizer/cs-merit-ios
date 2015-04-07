3a9fb5589d33ba50c67b31711ead39529a60c736
Max Beizer <max.beizer@gmail.com>
2015-04-06 20:33:33 -0500

    Add table view and cell

---
+++ cs-merit-ios/BadgeTypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BadgeTypeTableViewCell.swift
+//  cs-merit-ios
+//
+//  Created by Max Beizer on 4/6/15.
+//  Copyright (c) 2015 Max Beizer. All rights reserved.
+//
+
+import UIKit
+
+class BadgeTypeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
